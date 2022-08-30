431d881d0c1e6581e1ac78760ffbd4665128230f
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-30 13:46:32 +0100

    implement static and dynamic routing

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+const BlogPostsPage = () => {
+
+    const router = useRouter();
+    console.log(router.query);
+    
+    return (
+        <div>
+            <h1>The Blog Post</h1>
+        </div>
+    );
+}
+ 
+export default BlogPostsPage;
