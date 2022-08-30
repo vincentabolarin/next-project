1a91564c40f0e405aa1e5fa4649ac6f1a7efcbf1
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-30 16:58:17 +0100

    implement programatic navigation and set up 404 page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,17 @@ const ClientProjectsPage = () => {
     const router = useRouter();
     console.log(router.query);
 
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'vincent', clientprojectid: 'projecta'}
+        });
+    }
+
     return (
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
