dd247ba72dd782fcaf75fc670eb6c98f91ddffa2
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-30 15:21:08 +0100

    set up navigating to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+    const clients = [
+        {id: 'vincent', name: 'Vincent'},
+        {id: 'bassey', name: 'Bassey'}
+    ]
     return (
         <div>
             <h1>The Clients Page</h1>
+            <ul>
+                {clients.map(client => <li key={client.id}>
+                    <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                </li>)}
+            </ul>
         </div>
     );
 }
